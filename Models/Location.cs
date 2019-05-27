f94e8b77fae9b4ea1a2c60ed0eb44d340026a96d
Pawson Messi <exuberanteasy@outlook.com>
2019-05-27 12:05:06 +0800

    Create Location.cs

---
+++ Models/Location.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Location
+    {
+        public string City { get; set; }
+        public double[] Temperature { get; set; }
+    }
+}
